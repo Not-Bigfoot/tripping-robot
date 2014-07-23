0e7628c128bf507e99b87da8c20fc9d4cdc24132
James Budday <jwbudday@gmail.com>
2014-07-23 17:30:37 -0400

    started room, added resources folder

---
+++ CoolGame/src/main/java/DopeNerdInc/CoolGame/Room.java
@@ -0,0 +1,14 @@
+package DopeNerdInc.CoolGame;
+import java.io.File;
+public class Room 
+{
+	private final int roomHeight = 16;
+	private final int roomWidth = 16;
+	private int[][] movementArray = new int[roomWidth][roomHeight];
+	//where 0,0 is the bottom left
+	
+	public Room(int id)
+	{
+		
+	}
+}
